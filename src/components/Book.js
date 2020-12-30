5cabd9aa7c99d35c949121d72c4b39db3ff67e5a
Sakibul_Munna <sakibul.munnaa@gmail.com>
2020-12-30 20:29:21 +0600

    Some styles modified.

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Book = (props) => {
+    const ImageURL = props.book.volumeInfo.imageLinks;
     return (
-        <div className="col s12 m3" >
-            <div className="card huge" >
-                <div className="card-image">
-                    {props.book.volumeInfo.imageLinks === undefined ? (<img src={"https://picsum.photos/200/300"} style={{ width: 200, height: 300 }} alt="Hudai" />) :
-                        (<img src={props.book.volumeInfo.imageLinks.thumbnail} style={{ width: 245, height: 300 }} alt={"Book Title"} />)}
+        <div className="col s12 m3">
+            <div className="card large">
+                <div className="card-image" style={{ marginBottom: 10 }}>
+                    {ImageURL === undefined ? (<img src={"https://picsum.photos/200/300"}
+                        alt="hudai"
+                        style={{ width: "100", height: "100" }} />) :
+                        (<img src={props.book.volumeInfo.imageLinks.thumbnail} style={{ width: 245, height: 300 }} alt="Book Cover" />)}
                 </div>
                 <div>
-                    <span className="card-title" style={{ fontFamily: "Arial", marginLeft: 10, color: "black" }}>{props.book.volumeInfo.title}</span>
+                    <span className="card-title" style={{
+                        color: "black",
+                        fontSize: 18,
+                        fontFamily: 'monospace',
+                        position: 'absolute', marginTop: 10, marginLeft: 15, marginRight: 15
+                    }}>
+                        {props.book.volumeInfo.title}
+                    </span>
                 </div>
-                <div className="card-content">
-                    <p>{props.book.volumeInfo.authors}</p>
+                <div className="card-content"  style={{marginTop: 70, marginLeft: -6}}>
+                    <p style={{fontFamily:'cursive', color:'#11aabb'}}>{props.book.volumeInfo.authors}</p>
                 </div>
-                <div className="card-action">
+                <div className="card-action" >
                     <Link to={{
                         pathname: "/book/" + props.title,
                         title: props.title,
@@ -23,8 +33,7 @@ const Book = (props) => {
                         published: props.published,
                         image: props.image,
                         description: props.description,
-                        props: props.props,
-                        link: props.book.volumeInfo.previewLink
+                        props: props.props
                     }}>See Details</Link>
                 </div>
             </div>
